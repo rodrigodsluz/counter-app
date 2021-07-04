845edf932695cd6be51bc800b28777cff94c4a89
rodrigodsluz <rodrigodsluz@gmail.com>
2021-07-04 07:07:09 -0300

    Add fonts and set favicon icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -62,7 +62,12 @@ export default class ApplicationDocument extends Document<Props> {
           <link
             rel="icon"
             type="image/x-icon"
-            href="/favicons/favicon-d1.png"
+            href="/favicons/favicon.png"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body>
